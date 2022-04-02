03af90cbaad45fdd518441f0dc8bba7e831efdb2
mnl1337 <ssppiikkee@gmx.de>
2022-04-02 21:27:29 +0200

    variable naming added comments

--- collatz.py
+++ collatz.py
@@ -2,32 +2,45 @@ import time
 import csv
 l = [0]
 
+# the number range you want to calculate
+numStart = 1
+numEnd = 99999
+
+# creates a csv file with a dynamic name
+fileName = ''
+fileName = fileName.join('collatz' + str(numStart) + '_' + str(numEnd) + '.csv')
+
+# https://en.wikipedia.org/wiki/Collatz_conjecture
 def collatz():    
     global number
-    global counter
+    global steps
     if number % 2 == 0:
         number = number // 2
     elif number % 2 == 1:
         number = number * 3 + 1
-    counter = counter + 1
+    steps = steps + 1
     print (number)
 
-for number in range(10000, 99999, +1):
+# calculate the steps to reach 1 for every number defined at the start
+for number in range(numStart, numEnd, +1):
     print("Number: ",number)
-    counter = 0    
-    #print("Type in a number:" )
-    #number = int(input())
+    steps = 0    
     while number != 1:
         collatz()
-    print("Steps: ", counter)
-    if len(l)<counter:
-        for j in range(0, counter-len(l), +1):
+    print("Steps: ", steps)
+
+    # extends the list (if needed) and adds 1 were position=steps
+    if len(l)<steps:
+        for j in range(0, steps-len(l), +1):
             l.append(0)
-    l[counter-1] = l[counter-1] + 1
+    l[steps-1] = l[steps-1] + 1
     print()
-print(len(l))
+
+# prints the list
 for j in range(0, len(l), +1):
     print(j+1, ": ",l[j])
-with open('collatz.csv', 'w') as f:
+
+# saves the list in collatz.csv
+with open(fileName, 'w') as f:
     writer = csv.writer(f)
     writer.writerow(l)
